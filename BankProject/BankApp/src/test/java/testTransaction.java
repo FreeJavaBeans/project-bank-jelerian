c5baf255ba2e0cd4921eb4073d1e0b97e417a431
jelerian <68883206+jelerian@users.noreply.github.com>
2020-11-12 12:51:09 -0600

    add simple test case.

---
+++ BankProject/BankApp/src/test/java/testTransaction.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class testTransaction {
+
+	@Test
+	public void deposit()  {
+		double amount = -1;
+		assert(amount > 0);
+	}
+
+}
